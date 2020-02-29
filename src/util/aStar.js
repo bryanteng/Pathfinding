5ae399894eaaa0bbc81c92973551cfc7337f1a7f
Bryan Teng <bteng1226@gmail.com>
2020-02-29 15:45:13 -0600

    added solve path with aStar

--- src/util/aStar.js
+++ src/util/aStar.js
@@ -1,12 +1,4 @@
-class Node {
-  constructor(parent = undefined, pos = undefined) {
-    this.parent = parent;
-    this.pos = pos;
-    this.f = 0
-    this.g = 0
-    this.h = 0
-  }
-}
+const Node = require('../classes/Node.js')
 
 function aStar(maze, start, end){
   let startNode = new Node(undefined, start)
@@ -36,23 +28,23 @@ function aStar(maze, start, end){
           path.push(current.pos)
           current = current.parent
         }
-        for(node of open_list){
+        for(let node of open_list){
           // console.log(node);
-          board[node.pos[0]][node.pos[1]] = "o"
+          board[node.pos[0]][node.pos[1]] = "x"
         }
-        for(node of path){
+        for(let node of path){
           // console.log(node);
-          board[node[0]][node[1]] = "O"
+          board[node[0]][node[1]] = "X"
         }
         console.table(board);
-
+        return board
       return path.reverse()
     }
 
     let children = []
 
-    direction = [[0, -1], [0, 1], [-1, 0], [1, 0], [-1, -1], [-1, 1], [1, -1], [1, 1]]
-    for(dir of direction){
+    let direction = [[0, -1], [0, 1], [-1, 0], [1, 0], [-1, -1], [-1, 1], [1, -1], [1, 1]]
+    for(let dir of direction){
       let node_position = [currentNode.pos[0] + dir[0], currentNode.pos[1] + dir[1]]
       if(node_position[0] > maze.length - 1 || node_position[0] < 0 || node_position[1] > maze[maze.length-1].length - 1 || node_position[1] < 0) continue
       if(maze[node_position[0]][node_position[1]] != 0) continue
@@ -61,14 +53,14 @@ function aStar(maze, start, end){
       children.push(new_node)
     }
 
-    for(child of children){
-      for(closed_child of closed_list)
+    for(let child of children){
+      for(let closed_child of closed_list)
           if(child == closed_child) continue
       child.g = currentNode.g + 1
       child.h = ((child.pos[0] - endNode.pos[0]) ** 2) + ((child.pos[1] - endNode.pos[1]) ** 2)
       child.f = child.g + child.h
 
-      for(open_node of open_list){
+      for(let open_node of open_list){
           if(child == open_node && child.g > open_node.g) continue
       }
       open_list.push(child)
@@ -89,20 +81,16 @@ let y = 10
 let yArr = "0".repeat(y).split('').map(x=>+x)
 let arr = Array.from({length:x}).map(x=>yArr.slice())
 let board = arr.slice()
-console.log(board);
+
 //start pointx
-let startX = 1
-let startY = 1
+let startX = 9
+let startY = 9
 let start = [startX,startY]
 
-//current point
-let currentX = startX
-let currentY = startY
-let current = [startX, startY]
-
 //end point
-let endX = 9
-let endY = 8
+let endX = 0
+let endY = 0
 let end = [endX, endY]
-let path = aStar(board, start, end)
+// let path = aStar(board, start, end)
 // console.log(path,"done");
+module.exports = aStar
