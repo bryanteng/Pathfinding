6f949b296b318f59d315882764bd4ecaa22c1dcf
Bryan Teng <bteng1226@gmail.com>
2020-03-09 15:31:19 -0500

    working aStar and BFS algorithms, selectable algorithm radio buttons

--- src/util/BFS.js
+++ src/util/BFS.js
@@ -1,24 +1,24 @@
 const Node = require('../classes/Node.js').default
 
-var BFS = function (maze) {
+var BFS = function (maze, start, end) {
     //start pointx
-    let startX = 0
-    let startY = 0
-    let start = new Node(undefined, [startX, startY], 0)
+    // let startX = 0
+    // let startY = 0
+    let startNode = new Node(undefined, start)
     //end point
-    let endX = 5
-    let endY = 5
-    let end = new Node(undefined, [endX, endY])
-    const queue = [start];
+    // let endX = 5
+    // let endY = 5
+    let endNode = new Node(undefined, end)
+    const queue = [startNode];
     const directs = [[-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1]];
     const N = maze.length;
     const M = maze[0].length;
     const isValidPos = (x, y) => x >= 0 && x < N && y >= 0 && y < M;
     while (queue.length) {
         let current = queue.shift();
-        const { parent, pos: [x, y], dist } = current
+        const {parent, pos: [x, y], dist } = current
 
-        if (x == end.pos[0] && y == end.pos[1]) {
+        if (x == endNode.pos[0] && y == endNode.pos[1]) {
             let node = new Node(parent, [x, y], dist)
             let path = []
             while (node) {
@@ -43,7 +43,7 @@ var BFS = function (maze) {
             if (isValidPos(nextX, nextY) && maze[nextX][nextY] === 0) {
                 let new_node = new Node(current, [nextX, nextY], dist + 1)
                 queue.push(new_node);
-                maze[nextX][nextY] = 1;
+                maze[nextX][nextY] = "x";
             }
         }
     }
