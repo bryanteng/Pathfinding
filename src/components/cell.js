39a2146ee3291474a64b8ed2f0956022cd6362ac
Bryan Teng <bteng1226@gmail.com>
2020-03-10 15:09:52 -0500

    added coloring for path

--- src/components/cell.js
+++ src/components/cell.js
@@ -3,8 +3,16 @@ import React, { Component } from 'react'
 export default class Cell extends Component{
   render(){
     const {key, loc, block} = this.props
+    if(block){
+      var color = ""
+      if(block == "O"){
+        color = "LimeGreen"
+      }else if(block == "x"){
+        color = "#C80003"
+      }
+    }
     return(
-      <td className="block" key={key} loc={loc} onClick={this.props.onClick}>{block}</td>
+      <td className="block" key={key} loc={loc} style={{backgroundColor: color ? color : null}} onClick={this.props.onClick}>{block}</td>
     )
   }
 }
